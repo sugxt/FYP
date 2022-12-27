e1963b92e83f6ff831d431bb12a17df455e7fb93
sugxt <np03cs4s210024@heraldcollege.edu.np>
2022-12-27 19:45:13 +0545

    added routing and testing home page

--- server/index.js
+++ server/index.js
@@ -7,6 +7,17 @@ const app = express()
 
 const PORT = process.env.PORT || 5000;
 
+//Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//Routing
+
+app.get("/", (req,res) => {
+    res.send("Home Page");
+});
+
 //Connect to DB and Start Server
 mongoose
     .connect(process.env.MONGO_URI)
