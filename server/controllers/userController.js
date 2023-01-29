79dc102c7cdb9e33401e42f5fbd0e0c5e8ab7780
sugxt <np03cs4s210024@heraldcollege.edu.np>
2023-01-29 11:05:07 +0545

    Added function to access user data with safety

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -109,7 +109,38 @@ const loginUser = asyncHandler( async (req,res) => {
 
 });
 
+// Log the User Out
+
+const logout = asyncHandler (async (req,res) => {
+    res.cookie("token", "", {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(0), // Expires in the moment
+        sameSite:"none",
+        secure: true
+    });
+    return res.status(200).json({ message: "Successfully Logged Out" })
+});
+
+// Get User Profile
+
+const getUser = asyncHandler (async (req,res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        const {_id, name, email,photo,phone,role} = user
+        res.status(200).json({
+            _id, name, email, photo, phone,role
+        })
+    } else{
+        res.status(400)
+        throw new Error("User not found");
+    }
+});
+
 module.exports = {
     registerUser,
     loginUser,
+    logout,
+    getUser,
 };
--- server/route/userRoute.js
+++ server/route/userRoute.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } =  require("../controllers/userController");
+const { 
+    registerUser,
+    loginUser, 
+    logout,
+    getUser,
+    } =  require("../controllers/userController");
+const protect = require("../middleWare/authMiddleware");
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/logout", logout);
+router.get("/getuser",protect, getUser);
 
 module.exports = router;
